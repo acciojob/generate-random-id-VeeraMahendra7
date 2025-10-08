79d19984ba0b171fbba237316a2200f3b673b0d4
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-08 07:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	let str = "";
+	for(let i=0;i<l;i++){
+		str += char.charAt(Math.floor(Math.random()*char.length));
+	}
+	return str;
+
 }
 
 // Do not change the code below.
